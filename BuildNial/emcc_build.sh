ee1efe24d8c89729fc34cd0070a5f50954032a09
John Gibbons <gibbons.ja@gmail.com>
2023-09-11 18:23:10 +1000

    Added unicode and updated documentation

--- BuildNial/emcc_build.sh
+++ BuildNial/emcc_build.sh
@@ -2,7 +2,7 @@ mkdir -p build
 mkdir -p build/home
 cp -r home/* build/home/
 cd build
-cmake -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_TOOLCHAIN_FILE=/home/johng/Emscripten/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ../src
+cmake -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_TOOLCHAIN_FILE=/home/johng/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../src
 make
 echo 'Done'
 exit
